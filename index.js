72d5b424c8a878f3bfb8c167b7e1ae746d64cb55
Ian Wu <onlinemad@gmail.com>
2019-09-16 12:11:15 +0800

    Update cheerio to 1.0.0-rc.3. Refactor syntax to ES6 style.

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-var cheerio = require('cheerio');
-var fs 	 	= require('fs');
+const cheerio = require('cheerio')
 
-module.exports = function(table, options) {
+module.exports = function (table, options) {
 
-    var table = cheerio.load(table);
+  let table = cheerio.load(table)
 
-    var createMatrix = function(table) {
-        var matrix = [],
-            i = 0;
+  let createMatrix = function (table) {
+    let matrix = [],
+      i = 0
 
-        table("table tr").each(function() {
-            var j = 0;
-            matrix[i] = [];
+    table('table tr').each(function () {
+      let j = 0
+      matrix[i] = []
 
-            table(this).find('th').each(function() {
-                matrix[i][j] = table(this).text().trim().replace(/(\r\n|\n|\r)/gm, "");
-                j++;
-                return matrix;
-            });
+      table(this).find('th').each(function () {
+        matrix[i][j] = table(this).text().trim().replace(/(\r\n|\n|\r)/gm, '')
+        j++
+        return matrix
+      })
 
-            table(this).find('td').each(function() {
-                matrix[i][j] = '"' + table(this).text().trim() + '"';
-                j++;
-                return matrix;
-            });
+      table(this).find('td').each(function () {
+        matrix[i][j] = '"' + table(this).text().trim() + '"'
+        j++
+        return matrix
+      })
 
-            i++;
-        });
+      i++
+    })
 
-        return matrix;
+    return matrix
+  }
+
+  function createCsv(data) {
+    let csv = ''
+    for (let i = 0; i < data.length; i++) {
+      csv += data[i].join(',') + '\n'
     }
+    return csv
+  }
 
-    function createCsv(data) {
-        var csv = '';
-        for (var i = 0; i < data.length; i++) {
-            csv += data[i].join(',') + "\n";
-        }
-        return csv;
-    }
-    
-    return createCsv(createMatrix(table));
+  return createCsv(createMatrix(table))
 
 }
